9242c6bb989d03ecf5652fd9aab8d41d3591e0a5
Jacobhd <54602167+Jacobhd@users.noreply.github.com>
2020-03-17 09:34:36 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,20 +2,8 @@
 Treehouse FSJS Techdegree:
 project 1 - A Random Quote Generator
 ******************************************/
-
 // Study guide for this project - https://drive.google.com/file/d/1s5grutGuQFwJcQP8bFwEI69Q8FCkGdDk/view?usp=sharing
-
-
-/***
-  Create the array of quote objects and name it `quotes`.
-  Add at least five quote objects to the `quotes` array.
-  Give each quote object a `quote` and `source` property.
-  Add the `citation` property to at least one object in the array.
-  Add the `year` property to at least one object in the array.
-  Use console.log() to log your array of quotes to the console.
-***/
-
-//   An array of quote objects to display as HTML
+// An array of object's key and value pairs to display
 const quotes = [
   {
     quote:"True peace is not merely the absence of tension: it is the presence of justice.",
@@ -41,21 +29,15 @@ const quotes = [
   }
 ];
 
-/***
-  Create the `getRandomQuote` function to:
-   - Create a variable to store a random number
-   - Use the random number to `return` a random quote object from the `quotes` array.
-***/
-
-//  Function pulls a random object from the quotes array index
+// getRandomQuote function pulls a random object from the quotes array index
 function getRandomQuote(array) {
-/*  Stores a random number between 0 and the length of the quotes array
-    and assigns the value to the array index  */
+  /*  Stores a random number between 0 and the length of the quotes array
+  and assigns the value to the array index  */
   let quoteIndex = Math.floor(Math.random() * quotes.length);
   for (let i = 0; i < array.length; i++) {
     var randomQuote = array[quoteIndex];
   }
-//  Returns the random item and stores the result
+  //  Returns the random quote and stores the result
   return randomQuote;
 }
 let result = getRandomQuote(quotes);
@@ -75,17 +57,17 @@ console.log(result);
    - Set the `innerHTML` of the `quote-box` div to the HTML string.
 ***/
 
+
 //  Prints the random item into the html of the page
-function printQuote() {
-  getRandomQuote = randomQuote();
-//  Conditonal statement to add the citation and year properties only if they exist
-    if (randomQuote = quotes())
-
-
-  document.querySelector('main').innerHTML = quotes;
+function printQuote(message) {
+  var result = getRandomQuote();
+  var message = "<p class='quote'>" + result.quote + "</p>" +"<p class='source'>" + result.source + "</p>";
+  document.getElementById('quote-box').innerHTML = message;
 }
-//  Test
-console.log(printQuote);
+//  Conditonal statement to add the citation and year properties only if they exist
+
+//  Test console.log(printQuote);
+
 /***
   When the "Show another quote" button is clicked, the event listener
   below will be triggered, and it will call, or "invoke", the `printQuote`
