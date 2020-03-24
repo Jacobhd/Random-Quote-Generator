338abd562d86c06cb9caff2f4f7e14426da1d863
Jacobhd <54602167+Jacobhd@users.noreply.github.com>
2020-03-24 11:36:01 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ project 1 - A Random Quote Generator
 let quotes = [
   {
     quote:"True peace is not merely the absence of tension: it is the presence of justice.",
-    source:"Martin Luther King Jr.",
+    source:"Martin Luther King Jr",
     year:"1955"
   }, {
     quote:"It isn't enough to talk about peace. One must believe in it. And it isn't enough to believe in it. One must work at it.",
@@ -37,31 +37,23 @@ function getRandomQuote() {
    - Create a variable for the HTML string and set it equal to an empty string.
    - Use the HTML template in the instructions or the markup in the index.html file, AND
      the random quote vairable to build your HTML string.
-   - Add the quote and source section to the HTML string.
-   - Use an if statement to check for the citation property before adding it to the HTML string.
-   - Use an if statement to check for the year property before adding it to the HTML string.
-   - Don't forget to close that final `p` tag.
    - Set the `innerHTML` of the `quote-box` div to the HTML string.
 ***/
 
-
-//  printQuote function prints the random item into a message on the webpage
-
-function printQuote(message) { // message parameter
-  let result = getRandomQuote(); // Calls 'getRandomQuote' stores in result variable
-  var message = "<p class='quote'>" + result.quote + "</p>";
-  message += "<p class='source'>" + result.source;
-  if ( result.citation ) {
+//  printQuote function prints the random quote item into an HTML message on the webpage
+function printQuote(message) { // 'message' parameter
+  let result = getRandomQuote(); // Calls 'getRandomQuote' function stores in result variable
+  var message = "<p class='quote'>" + result.quote + "</p>"; // Creates 'message' variable with 'quote' property
+  message += "<p class='source'>" + result.source; // Adds 'source' property to HTML message
+  if ( result.citation ) { // Checks for 'citation' property, adds it to HTML message
     message += "<span class='citation'>" + result.citation + "</span>"
   };
-  if ( result.year ) {
+  if ( result.year ) { // Checks for 'year' property, adds it to HTML message
     message += "<span class='year'>" + result.year + "</span>"
   };
-  message += "</p"
-  document.getElementById('quote-box').innerHTML = message; // Using innerHTML property to interact with the browser
+  message += "</p" // Closing final 'p' tag
+  document.getElementById('quote-box').innerHTML = message; // Using innerHTML property to interact with the browser, displays HTML
 };
-
-
 
 /***
   When the "Show another quote" button is clicked, the event listener
